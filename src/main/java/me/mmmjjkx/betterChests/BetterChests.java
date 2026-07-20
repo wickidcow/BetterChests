6c436b595d0d1d89f40fd4cecae3b0aaa2822973
wickidcow <jaywago@gmail.com>
2026-07-20 19:40:12 -0400

    Update BetterChests java from github desktop to web

--- src/main/java/me/mmmjjkx/betterChests/BetterChests.java
+++ src/main/java/me/mmmjjkx/betterChests/BetterChests.java
@@ -1,21 +1,18 @@
 package me.mmmjjkx.betterChests;
 
-import io.github.schntgaispock.slimehud.SlimeHUD;
-import io.github.schntgaispock.slimehud.util.HudBuilder;
 import io.github.thebusybiscuit.slimefun4.api.SlimefunAddon;
-import io.github.thebusybiscuit.slimefun4.libraries.dough.updater.BlobBuildUpdater;
+import me.mmmjjkx.betterChests.integrations.SlimeHudIntegration;
 import me.mmmjjkx.betterChests.items.BCItems;
-import me.mmmjjkx.betterChests.items.chests.SimpleDrawer;
-import me.mmmjjkx.betterChests.items.chests.ie.IEStorageCache;
-import me.mmmjjkx.betterChests.items.chests.ie.IEStorageUnit;
 import me.mmmjjkx.betterChests.listeners.DrawerFixListener;
 import me.mmmjjkx.betterChests.utils.LanguageManager;
-import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
-import org.bukkit.Location;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.logging.Level;
+
+/** Main plugin entry point. */
 public final class BetterChests extends JavaPlugin implements SlimefunAddon {
+
     public static BetterChests INSTANCE;
 
     private LanguageManager languageManager;
@@ -23,44 +20,28 @@ public final class BetterChests extends JavaPlugin implements SlimefunAddon {
     @Override
     public void onEnable() {
         INSTANCE = this;
-
         saveDefaultConfig();
-        saveConfig();
 
         languageManager = new LanguageManager(this);
-
         BCItems.registerItems();
-
-        if (getServer().getPluginManager().isPluginEnabled("SlimeHUD")) {
-            SlimeHUD.getHudController().registerCustomHandler(SimpleDrawer.class, req -> {
-                SimpleDrawer d = (SimpleDrawer) req.getSlimefunItem();
-                Location loc = req.getLocation();
-                String itemName = LegacyComponentSerializer.legacyAmpersand().serialize(SimpleDrawer.getItemName(d.getStoringItem(loc)));
-                return "&7| &f" + itemName + " &7| &f" + HudBuilder.getAbbreviatedNumber(d.getStoringItemCount(loc)) + "/" + HudBuilder.getAbbreviatedNumber(d.getCapacity());
-            });
-
-            SlimeHUD.getHudController().registerCustomHandler(IEStorageUnit.class, req -> {
-                IEStorageUnit d = (IEStorageUnit) req.getSlimefunItem();
-                IEStorageCache cache = d.getCache(req.getLocation());
-                Location loc = req.getLocation();
-                String itemName = LegacyComponentSerializer.legacyAmpersand().serialize(SimpleDrawer.getItemName(d.getDisplayingItem(loc.getBlock())));
-                return "&7| &f" + itemName + " &7| &f" + HudBuilder.getAbbreviatedNumber(cache.getStored()) + "/" + HudBuilder.getAbbreviatedNumber(d.getCapacity());
-            });
-        }
-
         getServer().getPluginManager().registerEvents(new DrawerFixListener(), this);
 
-        getLogger().info("BetterChests has been enabled.");
-
-        if (getConfig().getBoolean("options.auto-update")) {
-            BlobBuildUpdater updater = new BlobBuildUpdater(this, getFile(), "BetterChests");
-            updater.start();
+        if (getServer().getPluginManager().isPluginEnabled("SlimeHUD")) {
+            try {
+                SlimeHudIntegration.register();
+                getLogger().info("SlimeHUD integration enabled.");
+            } catch (LinkageError | RuntimeException ex) {
+                getLogger().warning("SlimeHUD was found, but its API was incompatible. BetterChests will continue without HUD integration.");
+                getLogger().log(Level.WARNING, "SlimeHUD integration failure", ex);
+            }
         }
+
+        getLogger().info("BetterChests Albion fork enabled.");
     }
 
     @Override
     public void onDisable() {
-        getLogger().info("BetterChests has been disabled.");
+        getLogger().info("BetterChests Albion fork disabled.");
     }
 
     @Override
